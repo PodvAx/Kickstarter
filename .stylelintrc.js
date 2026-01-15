86cdcdfa5a83546c5f5a5cb629034b920f262357
Podvoiskyi Oleksandr <podvoyskiy01@gmail.com>
2026-01-15 16:25:32 +0200

    feat: add typography styles

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,17 @@
 module.exports = {
-  extends: "@mate-academy/stylelint-config",
-  plugins: [
-    "stylelint-scss"
-  ],
-  rules: {}
+  extends: '@mate-academy/stylelint-config',
+  plugins: ['stylelint-scss'],
+  rules: {
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        ignore: [
+          'inside-block',
+          'first-nested',
+          'blockless-after-same-name-blockless',
+        ],
+        ignoreAtRules: ['@else'],
+      },
+    ],
+  },
 };
