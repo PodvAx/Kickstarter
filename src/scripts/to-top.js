d41bdde484ea6bfa6cf8fb75621f3647f48e708a
Podvoiskyi Oleksandr <podvoyskiy01@gmail.com>
2026-02-05 23:00:32 +0200

    fix: to-top btn click event handler

--- src/scripts/to-top.js
+++ src/scripts/to-top.js
@@ -1,7 +1,9 @@
 const ID_BTN_TO_TOP = 'to-top';
 const CLASS_VISIBLE_BTN_TO_TOP = 'page__button--visible';
+const CLASS_LOGO = 'logo';
 
 const toTopBtn = document.getElementById(ID_BTN_TO_TOP);
+const logo = document.querySelector(`.${CLASS_LOGO}`);
 
 const toggleToTop = () => {
   const triggerPoint = window.innerHeight * 0.8;
@@ -20,6 +22,8 @@ const handleToTopBtnClick = (e) => {
     top: 0,
     behavior: 'smooth',
   });
+
+  logo.focus();
 };
 
 toTopBtn.addEventListener('click', handleToTopBtnClick);
